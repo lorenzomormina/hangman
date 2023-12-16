fb7a6475b17eb04091d6a705ca5cfc6dee90c0bf
Lorenzo Mormina <lmormina94@gmail.com>
2023-12-16 22:14:49 +0100

    a little bit of code cleaning

--- main.cpp
+++ main.cpp
@@ -27,45 +27,42 @@ struct Random
     int getNumber();
 };
 
-const SDL_Color BLACK{ 0, 0, 0,255 };
-const SDL_Color WHITE{ 255, 255, 255,255 };
-//const SDL_Color WINDOW_COLOR{ 255,255,255,255 };
-const SDL_Color BUTTON_COLOR{ 198, 140, 83,255 };
-const Vec2i BUTTON_SIZE{ 140, 40 };
-
-
 struct Text
 {
     string value;
     SDL_Texture* texture = nullptr;
+    Vec2i position;
 
     void setValue(const string& str);
+
     SDL_Rect getRect(int y);
-    void render(int y);
+    SDL_Rect getRect(int x, int y);
 
-    void Text::render(int x, int y);
-
-
-    SDL_Rect Text::getRect(int x, int y)
-    {
-        SDL_Rect r;
-        SDL_QueryTexture(texture, nullptr, nullptr, &r.w, &r.h);
-        r.y = y;
-        r.x = x;
-        return r;
-    }
+    void renderCenterH();
+    void render();
 };
 
 struct Button
 {
-    Vec2i position; // Position of the button
-    Vec2i size; // Size of the button
-    Text text; // Text on the button
-    SDL_Color backgroundColor; // Background color of the button
-    SDL_Color textColor; // Text color of the button
+    Vec2i position;
+    Vec2i size;
+    Text text;
+    SDL_Color backgroundColor;
+    SDL_Color textColor;
+
+    void render();
 };
 
-Vec2i windowSize{ 800,600 };
+
+const SDL_Color BLACK{ 0, 0, 0,255 };
+const SDL_Color WHITE{ 255, 255, 255,255 };
+const SDL_Color WINDOW_COLOR{ 230, 230, 230 };
+const SDL_Color BUTTON_COLOR{ 198, 140, 83,255 };
+const Vec2i BUTTON_SIZE{ 140, 40 };
+const Vec2i WINDOW_SIZE{ 800,600 };
+
+
+Random random;
 SDL_Window* window;
 SDL_Renderer* renderer;
 
@@ -81,9 +78,144 @@ Button btnReset;
 Button btnReveal;
 
 SDL_Texture* human[6];
+Vec2i humanPosition;
+TTF_Font* font24;
+TTF_Font* font14;
 
+bool running = true;
 bool didReveal = false;
 bool gameOver = false;
+
+
+void buildAssets();
+
+void loadWordList();
+bool isAlpha(string& str);
+void toUpper(string& str);
+
+SDL_Texture* loadTexture(const char* path);
+
+void processEvents();
+void prepareLetter(int scancode);
+void confirmLetter();
+void reveal();
+void resetGame();
+
+void draw();
+
+
+int main(int argc, char** argv)
+{
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+    TTF_Init();
+    IMG_Init(IMG_INIT_PNG);
+
+    window = SDL_CreateWindow(
+        "Hangman",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        WINDOW_SIZE.x, WINDOW_SIZE.y, 0
+    );
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+    buildAssets();
+
+    resetGame();
+
+    while (running)
+    {
+        processEvents();
+
+        SDL_SetRenderDrawColor(renderer, WINDOW_COLOR.r, WINDOW_COLOR.g, WINDOW_COLOR.b, WINDOW_COLOR.a);
+        SDL_RenderClear(renderer);
+        draw();
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_Quit();
+
+    return 0;
+}
+
+// [CHECK]
+void buildAssets()
+{
+    font24 = TTF_OpenFont("assets/RobotoMono-Regular.ttf", 24);
+    font14 = TTF_OpenFont("assets/RobotoMono-Regular.ttf", 14);
+
+    loadWordList();
+
+    random.setSize(wordList.size());
+
+    publicWord.position = { 0,50 };
+    label_wrongLetters.position = { 0,100 };
+    wrongLetters.position = { 0,150 };
+    label_currentLetter.position = { 0,200 };;
+    currentLetter.position = { 0,250 };
+
+    for (int i = 0; i < 6; ++i) {
+        string path = "assets/human_" + to_string(i) + ".png";
+        human[i] = loadTexture(path.c_str());
+    }
+
+    int w;
+    SDL_QueryTexture(human[0], nullptr, nullptr, &w, nullptr);
+    humanPosition.x = (WINDOW_SIZE.x - w) / 2;
+    humanPosition.y = 300;
+
+    btnReveal = {
+        { 20, 20 },
+        BUTTON_SIZE,
+        { "" },
+        BUTTON_COLOR,
+        BLACK
+    };
+
+    btnReveal.text.setValue("Reveal");
+
+    btnReset = {
+        { 20, 70 },
+        BUTTON_SIZE,
+        { "" },
+        BUTTON_COLOR,
+        BLACK
+    };
+
+    btnReset.text.setValue("Reset");
+}
+// [END CHECK]
+
+void loadWordList()
+{
+    ifstream infile("assets/words.txt");
+    string line;
+    while (getline(infile, line))
+    {
+        if (!isAlpha(line) || line.size() < 6)
+            continue;
+
+        toUpper(line);
+        wordList.push_back(line);
+    }
+}
+
+bool isAlpha(string& str)
+{
+    for (auto c : str)
+    {
+        if (!isalpha(c))
+            return false;
+    }
+    return true;
+}
+
+void toUpper(string& str)
+{
+    for (auto& c : str)
+    {
+        c = toupper(c);
+    }
+}
 
 SDL_Texture* loadTexture(const char* path)
 {
@@ -93,44 +225,93 @@ SDL_Texture* loadTexture(const char* path)
     return texture;
 }
 
-TTF_Font* font24;
-TTF_Font* font14;
-
-Random random;
-
-bool running = true;
-
-bool isAlpha(string& str);
-void toUpper(string& str);
-void loadWordList();
-
-void resetGame();
-void reveal();
-
-void handleEvent(SDL_Event& ev);
-void draw();
-
-
-
-void drawButton(Button& button)
+void processEvents()
 {
-    SDL_Rect buttonRect = { button.position.x, button.position.y, button.size.x, button.size.y };
-    SDL_SetRenderDrawColor(renderer, button.backgroundColor.r, button.backgroundColor.g, button.backgroundColor.b, button.backgroundColor.a);
-    SDL_RenderFillRect(renderer, &buttonRect);
+    SDL_Event ev;
+    while (SDL_PollEvent(&ev))
+    {
+        switch (ev.type)
+        {
+        case SDL_WINDOWEVENT:
+            if (ev.window.event == SDL_WINDOWEVENT_CLOSE)
+                running = false;
 
-    // get the size of the text
-    SDL_Rect textRect;
-    //SDL_QueryTexture(button.text.texture, nullptr, nullptr, &textRect.w, &textRect.h);
-    TTF_SizeText(font24, button.text.value.c_str(), &textRect.w, &textRect.h);
+        case SDL_KEYDOWN:
+        {
+            auto scancode = ev.key.keysym.scancode;
+            if (scancode >= SDL_SCANCODE_A && scancode <= SDL_SCANCODE_Z) {
+                prepareLetter(scancode);
+            }
+            else if (scancode == SDL_SCANCODE_RETURN) {
+                confirmLetter();
+            }
+            else if (scancode == SDL_SCANCODE_F2) {
+                reveal();
+            }
+            else if (scancode == SDL_SCANCODE_F3) {
+                resetGame();
+            }
+            break;
+        }
 
-    // center the text
-    textRect.x = button.position.x + (button.size.x - textRect.w) / 2;
-    textRect.y = button.position.y + (button.size.y - textRect.h) / 2;
+        case SDL_MOUSEBUTTONDOWN:
+        {
+            auto x = ev.button.x;
+            auto y = ev.button.y;
 
-    SDL_RenderCopy(renderer, button.text.texture, nullptr, &textRect);
+            if (x >= btnReset.position.x && x <= btnReset.position.x + btnReset.size.x &&
+                y >= btnReset.position.y && y <= btnReset.position.y + btnReset.size.y) {
+                resetGame();
+            }
+            else if (x >= btnReveal.position.x && x <= btnReveal.position.x + btnReveal.size.x &&
+                y >= btnReveal.position.y && y <= btnReveal.position.y + btnReveal.size.y) {
+                reveal();
+            }
 
+            break;
+        }
+
+        default:
+            break;
+        }
+    }
 }
 
+// [CHECK]
+void reveal()
+{
+    if (gameOver)
+        return;
+    publicWord.setValue(secretWord.value);
+    didReveal = true;
+    gameOver = true;
+    label_currentLetter.setValue("You Lose!");
+}
+
+void resetGame()
+{
+    gameOver = false;
+    didReveal = false;
+
+    auto idx = random.getNumber();
+    secretWord.setValue(wordList[idx]);
+
+    string pubw;
+    pubw.reserve();
+    for (auto c : secretWord.value)
+    {
+        pubw.push_back('_');
+    }
+
+    publicWord.setValue(pubw);
+
+    wrongLetters.setValue("");
+    label_wrongLetters.setValue("Wrong Letters (0):");
+    label_currentLetter.setValue("Current Letter:");
+    currentLetter.setValue("");
+
+    //cout << secretWord.value + "\n" << endl;
+}
 
 void prepareLetter(int scancode)
 {
@@ -181,7 +362,7 @@ void confirmLetter()
                 if (wrongLetters.value.size() / 2 > 5) {
                     label_currentLetter.setValue("You Lose!");
                 }
-                else if(wrongLetters.value.size() / 2 <= 5)
+                else if (wrongLetters.value.size() / 2 <= 5)
                     label_currentLetter.setValue("You Win!");
             }
         }
@@ -190,87 +371,26 @@ void confirmLetter()
 
     currentLetter.setValue("");
 }
+// [END CHECK]
 
 
-int main(int argc, char* argv[])
+Random::Random()
 {
-    // assuming no errors
-
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-    TTF_Init();
-
-    window = SDL_CreateWindow(
-        "Hangman",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        windowSize.x, windowSize.y, 0
-    );
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-
-    font24 = TTF_OpenFont("assets/RobotoMono-Regular.ttf", 24);
-    font14 = TTF_OpenFont("assets/RobotoMono-Regular.ttf", 14);
-
-    loadWordList();
-
-    random.setSize(wordList.size());
-
-
-    for (int i = 0; i < 6; ++i) {
-        string path = "assets/human_" + to_string(i) + ".png";
-        human[i] = loadTexture(path.c_str());
-    }
-
-    btnReset = {
-        { 20, 70 },
-        BUTTON_SIZE,
-        { "" },
-        BUTTON_COLOR,
-        BLACK
-    };
-
-    btnReset.text.setValue("Reset");
-
-    btnReveal = {
-        { 20, 20 },
-        BUTTON_SIZE,
-        { "" },
-        BUTTON_COLOR,
-        BLACK
-    };
-
-    btnReveal.text.setValue("Reveal");
-
-
-    resetGame();
-
-    // ---
-
-    while (running) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            handleEvent(event);
-        }
-
-        SDL_SetRenderDrawColor(renderer, 240, 240, 240, 255);
-        SDL_RenderClear(renderer);
-        draw();
-        SDL_RenderPresent(renderer);
-    }
-
-
-    // ---
-
-    TTF_CloseFont(font24);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    TTF_Quit();
-    SDL_Quit();
-
-    return 0;
+    gen.seed(rd());
 }
 
-// ---
+void Random::setSize(size_t size)
+{
+    distrib.param(std::uniform_int_distribution<size_t>::param_type(0, size - 1));
+}
 
+int Random::getNumber()
+{
+    return distrib(gen);
+}
+
+
+// [CHECK]
 void Text::setValue(const string& str)
 {
     value = str;
@@ -292,178 +412,78 @@ SDL_Rect Text::getRect(int y)
     SDL_Rect r;
     SDL_QueryTexture(texture, nullptr, nullptr, &r.w, &r.h);
     r.y = y;
-    r.x = (windowSize.x - r.w) / 2;
+    r.x = (WINDOW_SIZE.x - r.w) / 2;
     return r;
 }
 
-void Text::render(int x, int y)
+SDL_Rect Text::getRect(int x, int y)
 {
-    if (texture != nullptr) {
-        SDL_Rect r = getRect(x, y);
-        SDL_RenderCopy(renderer, texture, nullptr, &r);
-    }
+    SDL_Rect r;
+    SDL_QueryTexture(texture, nullptr, nullptr, &r.w, &r.h);
+    r.y = y;
+    r.x = x;
+    return r;
 }
 
-void Text::render(int y)
+void Text::renderCenterH()
 {
     if (texture != nullptr)
     {
-        SDL_Rect r = getRect(y);
+        SDL_Rect r;
+        SDL_QueryTexture(texture, nullptr, nullptr, &r.w, &r.h);
+        r.y = position.y;
+        r.x = (WINDOW_SIZE.x - r.w) / 2;
         SDL_RenderCopy(renderer, texture, nullptr, &r);
     }
 }
 
-// ---
-
-Random::Random()
+void Text::render()
 {
-    gen.seed(rd());
-}
-
-void Random::setSize(size_t size)
-{
-    distrib.param(std::uniform_int_distribution<size_t>::param_type(0, size - 1));
-}
-
-int Random::getNumber()
-{
-    return distrib(gen);
-}
-
-// ---
-
-bool isAlpha(string& str)
-{
-    for (auto c : str)
-    {
-        if (!isalpha(c))
-            return false;
-    }
-    return true;
-}
-
-void toUpper(string& str)
-{
-    for (auto& c : str)
-    {
-        c = toupper(c);
+    if (texture != nullptr) {
+        SDL_Rect r;
+        SDL_QueryTexture(texture, nullptr, nullptr, &r.w, &r.h);
+        r.x = position.x;
+        r.y = position.y;
+        SDL_RenderCopy(renderer, texture, nullptr, &r);
     }
 }
+// [END CHECK]
 
-void loadWordList()
+// [CHECK]
+void Button::render()
 {
-    ifstream infile("assets/words.txt");
-    string line;
-    while (getline(infile, line))
-    {
-        if (!isAlpha(line) || line.size() < 6)
-            continue;
+    SDL_Rect buttonRect = { position.x, position.y, size.x, size.y };
+    SDL_SetRenderDrawColor(renderer, backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a);
+    SDL_RenderFillRect(renderer, &buttonRect);
 
-        toUpper(line);
-        wordList.push_back(line);
-    }
+    SDL_Rect textRect;
+    TTF_SizeText(font24, text.value.c_str(), &textRect.w, &textRect.h);
+
+    // center the text
+    textRect.x = position.x + (size.x - textRect.w) / 2;
+    textRect.y = position.y + (size.y - textRect.h) / 2;
+
+    SDL_RenderCopy(renderer, text.texture, nullptr, &textRect);
+
 }
-
-// ---
-
-void resetGame()
-{
-    gameOver = false;
-    didReveal = false;
-
-    auto idx = random.getNumber();
-    secretWord.setValue(wordList[idx]);
-
-    string pubw;
-    pubw.reserve();
-    for (auto c : secretWord.value)
-    {
-        pubw.push_back('_');
-    }
-
-    publicWord.setValue(pubw);
-
-    wrongLetters.setValue("");
-    label_wrongLetters.setValue("Wrong Letters (0):");
-    label_currentLetter.setValue("Current Letter:");
-    currentLetter.setValue("");
-
-    //cout << secretWord.value + "\n" << endl;
-}
-
-// ---
-
-void handleEvent(SDL_Event& ev)
-{
-    switch (ev.type) {
-
-    case SDL_WINDOWEVENT:
-
-        switch (ev.window.event) {
-
-        case SDL_WINDOWEVENT_CLOSE:
-            running = false;
-            break;
-
-        }
-
-    case SDL_KEYDOWN:
-    {
-        auto scancode = ev.key.keysym.scancode;
-        if (scancode >= SDL_SCANCODE_A && scancode <= SDL_SCANCODE_Z) {
-            prepareLetter(scancode);
-        }
-        else if (scancode == SDL_SCANCODE_RETURN) {
-            confirmLetter();
-        }
-        else if (scancode == SDL_SCANCODE_F2) {
-            reveal();
-        }
-        else if (scancode == SDL_SCANCODE_F3) {
-            resetGame();
-        }
-        break;
-    }
-
-    case SDL_MOUSEBUTTONDOWN:
-    {
-        auto x = ev.button.x;
-        auto y = ev.button.y;
-
-        if (x >= btnReset.position.x && x <= btnReset.position.x + btnReset.size.x &&
-            y >= btnReset.position.y && y <= btnReset.position.y + btnReset.size.y) {
-            resetGame();
-        }
-        else if (x >= btnReveal.position.x && x <= btnReveal.position.x + btnReveal.size.x &&
-            y >= btnReveal.position.y && y <= btnReveal.position.y + btnReveal.size.y) {
-            reveal();
-        }
-
-        break;
-    }
-
-    default:
-        break;
-    }
-}
-
+// [END CHECK]
 
 void draw()
 {
-    publicWord.render(50);
-    label_wrongLetters.render(100);
-    wrongLetters.render(150);
-    label_currentLetter.render(200);
-    currentLetter.render(250);
+    publicWord.renderCenterH();
+    label_wrongLetters.renderCenterH();
+    wrongLetters.renderCenterH();
+    label_currentLetter.renderCenterH();
+    currentLetter.renderCenterH();
 
     SDL_Rect r;
     SDL_QueryTexture(human[0], nullptr, nullptr, &r.w, &r.h);
-    r.x = (windowSize.x - r.w) / 2;
+    r.x = (WINDOW_SIZE.x - r.w) / 2;
     r.y = 300;
 
     int humanIndex;
     if (didReveal) {
-                humanIndex = 5;
+        humanIndex = 5;
     }
     else {
         humanIndex = wrongLetters.value.size() / 2 > 5 ? 5 : wrongLetters.value.size() / 2;
@@ -471,17 +491,8 @@ void draw()
 
     SDL_RenderCopy(renderer, human[humanIndex], nullptr, &r);
 
-    drawButton(btnReset);
-    drawButton(btnReveal);
+    btnReveal.render();
+    btnReset.render();
 }
 
 
-void reveal()
-{
-    if(gameOver)
-        return;
-    publicWord.setValue(secretWord.value);
-    didReveal = true;
-    gameOver = true;
-    label_currentLetter.setValue("You Lose!");
-}
